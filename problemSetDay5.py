7f48721f1f8ab7941e2f9fb6ea5ef43e603ed6f6
elenarubenstein <elenarubenstein1@gmail.com>
2019-03-09 17:09:53 +0000

    day 5

--- problemSetDay5.py
+++ problemSetDay5.py
@@ -1,1 +1,16 @@
 # File name: problemSetDay5.py
+for i in range (5):
+    print(i)
+for p in range (5,8):
+    print(p)
+for q in range(0,31,5):
+    print(q)
+print("----")
+e=1
+while e<5:
+    print(e)
+    e+=1
+print("----")
+c=("*")
+for c in range(1,8,2):
+    print (c)
